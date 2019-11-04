ad8b9056093631466e20c0d1ea700d9016576539
Wilco Fiers <wilco.fiers@deque.com>
2019-11-04 22:00:33 +0100

    chore: add vuetify

--- src/main.ts
+++ src/main.ts
@@ -1,20 +1,20 @@
 import Vue from "vue";
-import { auth } from './firebase';
+import { auth } from "./firebase";
 import App from "./App.vue";
 import router from "./router";
 import store from "./store";
+import vuetify from "./plugins/vuetify";
 
 // Vue.config.productionTip = false;
 
 auth.onAuthStateChanged(user => {
   if (user) {
-    store.dispatch('autoSignIn', user)
+    store.dispatch("autoSignIn", user);
   }
   new Vue({
     router,
     store,
+    vuetify,
     render: h => h(App)
   }).$mount("#app");
-})
-
-
+});
