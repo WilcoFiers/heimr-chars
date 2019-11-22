2a01de61f81b2268290aa8646a1c4075c3d2105c
Wilco Fiers <wilco.fiers@deque.com>
2019-11-22 19:52:03 +0100

    fix(a11y): page-not-found missing img alt

--- src/views/PageNotFound.vue
+++ src/views/PageNotFound.vue
@@ -4,10 +4,11 @@
     <p class="subheading font-weight-regular">
       Please wait while our gnomes are working on it.
     </p>
-    <img
-      width="300"
-      src="https://vignette.wikia.nocookie.net/pdd/images/1/15/Tinker_1.jpg/revision/latest?cb=20140814215025"
-      alt
-    />
+
+    <v-row justify="center">
+      <v-col :cols="4">
+        <v-img :src="require('../assets/gnome.jpg')" />
+      </v-col>
+    </v-row>
   </v-container>
 </template>
