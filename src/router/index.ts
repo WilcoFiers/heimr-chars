7ae066900e28214536c836767834813692c0bcb5
Wilco Fiers <wilco.fiers@deque.com>
2020-01-02 20:24:18 +0100

    fix: scroll after navigation

--- src/router/index.ts
+++ src/router/index.ts
@@ -30,7 +30,16 @@ const routes = [
 ];
 
 const router = new VueRouter({
-  routes
+  routes,
+  scrollBehavior(to, from, savedPosition) {
+    if (savedPosition) {
+      return savedPosition;
+    } else if (to.hash) {
+      return { selector: to.hash };
+    } else {
+      return { x: 0, y: 0 };
+    }
+  }
 });
 
 export default router;
