ecfb8ad3f79298ce6a97ed370af94bb86112ac60
Wilco Fiers <wilco.fiers@deque.com>
2020-01-26 20:23:24 +0100

    fix: always update manifest when online

--- src/service-worker.js
+++ src/service-worker.js
@@ -1,27 +1,35 @@
 if (workbox) {
-    // adjust log level for displaying workbox logs
-    workbox.core.setLogLevel(workbox.core.LOG_LEVELS.debug)
+  // adjust log level for displaying workbox logs
+  workbox.core.setLogLevel(workbox.core.LOG_LEVELS.debug)
 
-    workbox.routing.registerNavigationRoute('/index.html')
+  workbox.routing.registerNavigationRoute('/index.html')
 
-    // Cache the Google Fonts stylesheets with a stale-while-revalidate strategy.
-    workbox.routing.registerRoute(
-      /^https:\/\/fonts\.googleapis\.com/,
-      new workbox.strategies.StaleWhileRevalidate({
-        cacheName: 'google-fonts-stylesheets',
-      })
-    );
+  // Cache the Google Fonts stylesheets with a stale-while-revalidate strategy.
+  workbox.routing.registerRoute(
+    /^https:\/\/fonts\.googleapis\.com/,
+    new workbox.strategies.StaleWhileRevalidate({
+      cacheName: 'google-fonts-stylesheets',
+    })
+  );
 
-    // Cache the Material Design icon stylesheet.
-    workbox.routing.registerRoute(
-      /^https:\/\/cdn\.jsdelivr\.net/ ,
-      new workbox.strategies.StaleWhileRevalidate({
-        cacheName: 'material-design-icon-stylesheet',
-      })
-    );
+  // Cache the Material Design icon stylesheet.
+  workbox.routing.registerRoute(
+    /^https:\/\/cdn\.jsdelivr\.net/,
+    new workbox.strategies.StaleWhileRevalidate({
+      cacheName: 'material-design-icon-stylesheet',
+    })
+  );
 
-    // apply precaching. In the built version, the precacheManifest will
-    // be imported using importScripts (as is workbox itself) and we can 
-    // precache this. This is all we need for precaching
-    workbox.precaching.precacheAndRoute(self.__precacheManifest);
+  // Always look for updates
+  workbox.routing.registerRoute(
+    /manifest\.json$/,
+    new workbox.strategies.NetworkFirst({
+      cacheName: 'manifest-json'
+    })
+  );
+
+  // apply precaching. In the built version, the precacheManifest will
+  // be imported using importScripts (as is workbox itself) and we can 
+  // precache this. This is all we need for precaching
+  workbox.precaching.precacheAndRoute(self.__precacheManifest);
 }
